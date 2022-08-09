a3a64afb5250ae21478689ef208757816a2ac4f1
meisterjustice <eziefulejustice@gmail.com>
2022-08-10 02:03:01 +0400

    chore: test events

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -5,7 +5,7 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
 !developmentChains.includes(network.name)
     ? describe.skip
     : describe("Raffle", async () => {
-          let raffle, vrfCoordinatorV2Mock, raffleEntranceFee, deployer
+          let raffle, vrfCoordinatorV2Mock, raffleEntranceFee, deployer, interval
           const chainId = network.config.chainId
 
           beforeEach(async () => {
@@ -14,6 +14,7 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
               raffle = await ethers.getContract("Raffle", deployer)
               vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
               raffleEntranceFee = await raffle.getEntranceFee()
+              interval = await raffle.getInterval()
           })
 
           describe("constructor", async () => {
@@ -38,5 +39,23 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
 
                   assert.equal(playerFromContract, deployer)
               })
+              it("emits event on enter", async () => {
+                  await expect(raffle.enterRaffle({ value: raffleEntranceFee })).to.emit(
+                      raffle,
+                      "RaffleEnter"
+                  )
+              })
+              //   it("doesn't allow entrance when raffle is calculating", async () => {
+              //       await raffle.enterRaffle({ value: raffleEntranceFee })
+              //       // for a documentation of the methods below, go here: https://hardhat.org/hardhat-network/reference
+              //       await network.provider.send("evm_increaseTime", [interval.toNumber() + 1])
+              //       await network.provider.request({ method: "evm_mine", params: [] })
+              //       // we pretend to be a keeper for a second
+              //       await raffle.performUpkeep([]) // changes the state to calculating for our comparison below
+              //       await expect(raffle.enterRaffle({ value: raffleEntranceFee })).to.be.revertedWith(
+              //           // is reverted as raffle is calculating
+              //           "Raffle__RaffleNotOpen"
+              //       )
+              //   })
           })
       })
